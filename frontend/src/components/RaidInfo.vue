a887a75d0f15062721577e36b76fe2192900699e
matheusclmb <ccolombomatheus@gmail.com>
2022-08-22 21:10:37 +0100

    Finished raid component

--- frontend/src/components/RaidInfo.vue
+++ frontend/src/components/RaidInfo.vue
@@ -14,7 +14,10 @@
 
   <div class="tabs is-toggle is-centered">
     <ul>
-      <li class="is-active">
+      <li
+        v-on:click="getActive('normal')"
+        v-bind:class="{ 'is-active': isActive === 'normal' }"
+      >
         <a>
           <span class="icon is-small"
             ><i class="fas fa-smile" aria-hidden="true"></i
@@ -22,7 +25,10 @@
           <span>Normal</span>
         </a>
       </li>
-      <li>
+      <li
+        v-on:click="getActive('hard')"
+        v-bind:class="{ 'is-active': isActive === 'hard' }"
+      >
         <a>
           <span class="icon is-small"
             ><i class="fa fa-ghost" aria-hidden="true"></i
@@ -30,7 +36,10 @@
           <span>Hard</span>
         </a>
       </li>
-      <li>
+      <li
+        v-on:click="getActive('inferno')"
+        v-bind:class="{ 'is-active': isActive === 'inferno' }"
+      >
         <a>
           <span class="icon is-small"
             ><i class="fas fa-fire" aria-hidden="true"></i
@@ -45,6 +54,7 @@
     <h1 class="is-size-4 has-text-centered">Times</h1>
     <div class="is-flex is-align-items-center">
       <table
+        v-if="isActive === 'normal'"
         class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-text-centered"
       >
         <thead>
@@ -66,17 +76,63 @@
             <td>1468</td>
             <td>12:41</td>
             <td>Yes</td>
-            <td>Click</td>
+            <td><a href="#">Click</a></td>
           </tr>
+        </tbody>
+      </table>
+
+      <table
+        v-if="isActive === 'hard'"
+        class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-text-centered"
+      >
+        <thead>
+          <tr class="has-text-centered">
+            <th>Position</th>
+            <th>Name</th>
+            <th>iLvl</th>
+            <th>Time</th>
+            <th>Deathless</th>
+            <th>Proof</th>
+          </tr>
+        </thead>
+        <tbody>
           <tr>
-            <td>2</td>
+            <td>1</td>
             <td>
-              Player - Mario - Kalleb - Otto - Unpleasant - Persa - Bob - Gus
+              Jonas - Mario - Kalleb - Otto - Unpleasant - Persa - Bob - Gus
             </td>
             <td>1468</td>
             <td>12:41</td>
             <td>Yes</td>
-            <td>Click</td>
+            <td><a href="#">Click</a></td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table
+        v-if="isActive === 'inferno'"
+        class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-text-centered"
+      >
+        <thead>
+          <tr class="has-text-centered">
+            <th>Position</th>
+            <th>Name</th>
+            <th>iLvl</th>
+            <th>Time</th>
+            <th>Deathless</th>
+            <th>Proof</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              Iferno - Mario - Kalleb - Otto - Unpleasant - Persa - Bob - Gus
+            </td>
+            <td>1468</td>
+            <td>12:41</td>
+            <td>Yes</td>
+            <td><a href="#">Click</a></td>
           </tr>
         </tbody>
       </table>
@@ -86,7 +142,17 @@
 
 <script>
 export default {
-  name: "RaidInfo",
+  name: "Raidinfo",
+  data() {
+    return {
+      isActive: "normal",
+    };
+  },
+  methods: {
+    getActive(difficulty) {
+      this.isActive = difficulty;
+    },
+  },
 };
 </script>
 
