dfaf37a55f3c19ac8eb4ecebdcfb0adb45840a8d
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-22 19:20:07 +0100

    Added Argos view

---
+++ frontend/src/views/Argos.vue
@@ -0,0 +1,14 @@
+<template>
+  <RaidInfo msg="sex" />
+</template>
+
+<script>
+import RaidInfo from "@/components/RaidInfo.vue";
+
+export default {
+  name: "Raid",
+  components: {
+    RaidInfo,
+  },
+};
+</script>
