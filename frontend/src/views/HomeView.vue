75e954aa08e79eccd5004ecabb6bfe05455b26bf
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-22 19:19:45 +0100

    Changes in home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box has-text-centered has-background-dark has-text-primary">
+      <h1 class="is-size-3">Welcome to LOA.runs!</h1>
+      <p>
+        This website was created to track the best times for each raid. Submit a
+        run and dispute for the first place!
+      </p>
+      <p>PS: Website still in development.</p>
+    </div>
+  </div>
+
+  <div class="topthree has-text-centered">
+    <h1 class="is-size-3 has-text-centered">Last Update</h1>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">New website structure</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <p>
+            The website is now split into two different pages. The first page is
+            the home page, and the second page is the abyss raid page.
+          </p>
+          <time>11:09 PM - 22 Aug 2022</time>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
