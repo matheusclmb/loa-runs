1b5fb524753d9d1f05960ae914478914922cc499
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-23 16:16:16 +0100

    Add New URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path("", home, name="home"),
     path("abyssraids/", abyssraids.as_view(), name="abyssraids"),
+    path("legionraids/", legionraids.as_view(), name="legionraids"),
+    path("runs/", runs.as_view(), name="runs"),
+    path("createrun/", CreateRun.as_view(), name="create"),
 ]
