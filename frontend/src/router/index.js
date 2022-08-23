b5a876a4079c211d21348d03d60d79551adabe46
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-23 15:01:57 +0100

    New Routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import Help from "../views/Help.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Argos from "../views/Argos.vue";
+import Valtan from "../views/Valtan.vue";
+import Vykas from "../views/Vykas.vue";
+import Submit from "../views/Submit.vue";
 
 const routes = [
   {
@@ -15,9 +18,24 @@ const routes = [
     component: Help,
   },
   {
+    path: "/submitrun",
+    name: "submitrun",
+    component: Submit,
+  },
+  {
     path: "/abyss/argos",
     name: "argos",
     component: Argos,
+  },
+  {
+    path: "/legion/valtan",
+    name: "valtan",
+    component: Valtan,
+  },
+  {
+    path: "/legion/vykas",
+    name: "vykas",
+    component: Vykas,
   },
 ];
 
