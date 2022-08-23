3fbb6abb4c491e7e27986286f5f462f5b7d5dfe4
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-23 15:01:27 +0100

    Refactored Argos page

--- frontend/src/views/Argos.vue
+++ frontend/src/views/Argos.vue
@@ -1,14 +1,37 @@
 <template>
-  <RaidInfo />
+  <RaidInfo v-bind:raid="raid" />
 </template>
 
 <script>
 import RaidInfo from "@/components/RaidInfo.vue";
+import axios from "axios";
 
 export default {
-  name: "Raid",
+  name: "Argos",
+  data() {
+    return {
+      raid: [],
+    };
+  },
   components: {
     RaidInfo,
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios
+        .get("/api/v1/abyssraids/")
+        .then((response) => {
+          this.raid = response.data[0];
+
+          document.title = this.raid.name + " | LOA.runs";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
