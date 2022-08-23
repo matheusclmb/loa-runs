2c28ada8e4417f0276ca228c072844c5dfcf4deb
unpleasantpersa <ccolombomatheus@gmail.com>
2022-08-23 16:16:22 +0100

    Added New Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .serializers import *
 from rest_framework.views import APIView
+from rest_framework import generics 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -15,3 +16,20 @@ class abyssraids(APIView):
         abyssraid = AbyssRaids.objects.all()
         serializer = AbyssRaidsSerializer(abyssraid, many=True)
         return Response(serializer.data)
+
+class legionraids(APIView):
+    def get(self, request):
+        legionraid = LegionRaids.objects.all()
+        serializer = LegionRaidsSerializer(legionraid, many=True)
+        return Response(serializer.data)
+
+
+class runs(APIView):
+    def get(self, request):
+        run = Runs.objects.all()
+        serializer = RunsSerializer(run, many=True)
+        return Response(serializer.data)
+
+class CreateRun(generics.CreateAPIView):
+    queryset = Runs.objects.all()
+    serializer_class = RunsSerializer
