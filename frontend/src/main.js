d1e9cf27df5ca61d4499a3609620574278e0262d
matheusclmb <ccolombomatheus@gmail.com>
2022-08-24 23:12:05 +0100

    Import of Vue Toast

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
+import ToastPlugin from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
 
 axios.defaults.baseURL = "http://127.0.0.1:8000";
 
