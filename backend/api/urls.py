3b67c9b060d268ee83ffffae9fabbbda2f30d137
matheusclmb <ccolombomatheus@gmail.com>
2022-08-27 01:19:22 +0100

    URLS/Views changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,20 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("abyssraids/argos", ArgosRuns.as_view(), name="argosruns"),
     path("abyssraids/", abyssraids.as_view(), name="abyssraids"),
+    path(
+        "legionraids/valtan/normal/", ValtanRunsNormal.as_view(), name="valtan_normal"
+    ),
+    path("legionraids/valtan/hard/", ValtanRunsHard.as_view(), name="valtan_hard"),
+    path(
+        "legionraids/valtan/inferno/",
+        ValtanRunsInferno.as_view(),
+        name="valtan_inferno",
+    ),
+    path("legionraids/vykas/normal/", VykasRunsNormal.as_view(), name="vykas_normal"),
+    path("legionraids/vykas/hard/", VykasRunsHard.as_view(), name="vykas_hard"),
+    path("legionraids/vykas/inferno/", VykasRunsInferno.as_view(), name="vykas_hard"),
     path("legionraids/", legionraids.as_view(), name="legionraids"),
     path("runs/", runs.as_view(), name="runs"),
     path("createrun/", CreateRun.as_view(), name="create"),
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .serializers import *
 from rest_framework.views import APIView
-from rest_framework import generics 
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -17,6 +17,7 @@ class abyssraids(APIView):
         serializer = AbyssRaidsSerializer(abyssraid, many=True)
         return Response(serializer.data)
 
+
 class legionraids(APIView):
     def get(self, request):
         legionraid = LegionRaids.objects.all()
@@ -30,6 +31,57 @@ class runs(APIView):
         serializer = RunsSerializer(run, many=True)
         return Response(serializer.data)
 
+
 class CreateRun(generics.CreateAPIView):
     queryset = Runs.objects.all()
     serializer_class = RunsSerializer
+
+
+# RAIDS SPECIFICS
+class ArgosRuns(APIView):
+    def get(self, request):
+        argos = Runs.objects.filter(raid_name="Argos")
+        serializer = RunsSerializer(argos, many=True)
+        return Response(serializer.data)
+
+
+class ValtanRunsNormal(APIView):
+    def get(self, request):
+        valtan = Runs.objects.filter(raid_name="Valtan").filter(difficulty="Normal")
+        serializer = RunsSerializer(valtan, many=True)
+        return Response(serializer.data)
+
+
+class ValtanRunsHard(APIView):
+    def get(self, request):
+        valtan = Runs.objects.filter(raid_name="Valtan").filter(difficulty="Hard")
+        serializer = RunsSerializer(valtan, many=True)
+        return Response(serializer.data)
+
+
+class ValtanRunsInferno(APIView):
+    def get(self, request):
+        valtan = Runs.objects.filter(raid_name="Valtan").filter(difficulty="Inferno")
+        serializer = RunsSerializer(valtan, many=True)
+        return Response(serializer.data)
+
+
+class VykasRunsNormal(APIView):
+    def get(self, request):
+        vykas = Runs.objects.filter(raid_name="Vykas").filter(difficulty="Normal")
+        serializer = AbyssRaidsSerializer(vykas, many=True)
+        return Response(serializer.data)
+
+
+class VykasRunsHard(APIView):
+    def get(self, request):
+        vykas = Runs.objects.filter(raid_name="Vykas").filter(difficulty="Hard")
+        serializer = RunsSerializer(vykas, many=True)
+        return Response(serializer.data)
+
+
+class VykasRunsInferno(APIView):
+    def get(self, request):
+        vykas = Runs.objects.filter(raid_name="Vykas").filter(difficulty="Inferno")
+        serializer = RunsSerializer(vykas, many=True)
+        return Response(serializer.data)
